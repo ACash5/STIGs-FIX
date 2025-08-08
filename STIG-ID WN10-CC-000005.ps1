c58c050988ab8eb591a9834ad1054ca11b7b2bf6
ACash5 <audiewilliams18@gmail.com>
2025-08-07 20:51:10 -0500

    Update STIG-ID WN10-CC-000005.ps1

--- STIG-ID WN10-CC-000005.ps1
+++ STIG-ID WN10-CC-000005.ps1
@@ -11,7 +11,7 @@
     Version         : 1.0
     CVEs            : N/A
     Plugin IDs      : N/A
-    STIG-ID         : WN10-CC-000005
+    STIG-ID         : WN10-SO-000005
 
 .TESTED ON
     Date(s) Tested  : 
@@ -22,9 +22,22 @@
 .USAGE
     Put any usage instructions here.
     Example syntax:
-    PS C:\> .\__remediation_template(STIG-ID-WN10-CC-000005).ps1 NoLockScreenCamera
+    PS C:\> .\__remediation_template(STIG-ID-WN10-SO-000005).ps1 
 #>
-# YOUR CODE GOES HERE$path = "HKLM\SOFTWARE\Policies\Microsoft\Windows\Personalization"
+# Fix for WN10-SO-000005 - Require password on wake after sleep (on battery)
 
-New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Personalization" -Force
-New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Personalization" -Name "NoLockScreenCamera" -Value 1 -PropertyType DWord -Force
+# GUID for "Require a password on wakeup" - On Battery
+$subgroupGuid = "4f971e89-eebd-4455-a8de-9e59040e7347"
+$settingGuid = "abfc2519-3608-4c2a-94ea-171b0ed546ab"
+
+# Set to require password (1 = Require password)
+powercfg /SETDCVALUEINDEX SCHEME_CURRENT $subgroupGuid $settingGuid 1
+
+# Apply the change
+powercfg /SETACTIVE SCHEME_CURRENT
+
+Write-Output "WN10-SO-000005 fix applied: Password required on wake from sleep (on battery)."
+#
+#Verify the Setting
+#powercfg /QUERY SCHEME_CURRENT 
+#The Current AC/DC Power Setting Index should show 0x00000001
