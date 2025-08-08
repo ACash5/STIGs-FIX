8003ab5d03186cb3019bf21dac9d1266d8c3df33
ACash5 <audiewilliams18@gmail.com>
2025-08-07 20:56:38 -0500

    Update STIG-ID WN10-CC-000005.ps1

--- STIG-ID WN10-CC-000005.ps1
+++ STIG-ID WN10-CC-000005.ps1
@@ -1,7 +1,7 @@
 <<#
 .SYNOPSIS
-    This PowerShell script ensures that the maximum size of the Windows Application event log is at least 32768 KB (32 MB).
-
+    This setting prevents unauthorized use of the camera from the lock screen, reducing the risk of privacy violations or information leakage.
+    
 .NOTES
     Author          : Audie Williams
     LinkedIn        : www.linkedin.com/in/audiewilliams
